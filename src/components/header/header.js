cdc84299e784bc4f611d5054a3faa3fe66532dc7
ivan minutillo <minutillo.ivan@gmail.com>
2017-03-11 17:45:08 +0100

    home

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,40 +4,40 @@ import {
 } from 'react-router-dom'
 
 const Header = () => (
-        <section className='main_header'>
-            <div className='header_left'>
-                <span className='header_logo'></span>
-                <NavLink to='/'>
-                    <h1 className='header_title'>Lipsum</h1>
-                </NavLink>
-            </div>
-            <div className='header_menu'>
-                <ul className='menu_list'>
-                    <li className='list_item'>
-                        <NavLink activeClassName='active' to='/about'>About</NavLink>
-                    </li>
-                    <li className='list_item'>
-                        <NavLink activeClassName='active' to='/joinus'>Partecipa</NavLink>
-                    </li>
-                    <li className='list_item'>
-                        <NavLink activeClassName='active' to='/services'>Servizi</NavLink>
-                    </li>
-                    <li className='list_item'>
-                        <a href='#'>Blog</a>
-                    </li>
-                    <li className='list_item'>
-                        <a href='#'>Forum</a>
-                    </li>
-                    <li className='list_item'>
-                        <NavLink to='/contactus'>Contact us</NavLink>
-                    </li>
-                </ul>
-            </div>
-            <div className='header_right'>
-                <a href='#'>accedi</a>
-                <span className='icon-login'></span>
-            </div>
-        </section>
+    <section className='main_header'>
+        <div className='header_left'>
+            <span className='header_logo'></span>
+            <NavLink to='/'>
+                <h1 className='header_title'>Lipsum</h1>
+            </NavLink>
+        </div>
+        <div className='header_menu'>
+            <ul className='menu_list'>
+                <li className='list_item'>
+                    <NavLink activeClassName='active' to='/about'>About</NavLink>
+                </li>
+                <li className='list_item'>
+                    <NavLink activeClassName='active' to='/joinus'>Partecipa</NavLink>
+                </li>
+                <li className='list_item'>
+                    <NavLink activeClassName='active' to='/services'>Servizi</NavLink>
+                </li>
+                <li className='list_item'>
+                    <a href='#'>Blog</a>
+                </li>
+                <li className='list_item'>
+                    <a href='#'>Forum</a>
+                </li>
+                <li className='list_item'>
+                    <NavLink to='/contactus'>Contact us</NavLink>
+                </li>
+            </ul>
+        </div>
+        <div className='header_right'>
+            <a href='#'>accedi</a>
+            <span className='icon-login'></span>
+        </div>
+    </section>
 )
 
 export default Header
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const Home = () => (
-    <h1>home</h1>
+    <section className='home row'>
+        <div className='medium-7 columns medium-centered'>
+            <div className='home_header'>
+                <h2 className='header_title'>PER L’AUTOGESTIONE E L’AUTODETERMINAZIONE DIGITALE</h2>
+            </div>
+            <div className='content'>
+                <div className='content_text'>
+                    <p>One way to announce or promote a certain new product or special events is perhaps through using of vinyl banners. Large or small size of printing these vinyl banners are can be able to print and in many types of weather it can hold up extremely well.</p>
+                    <p>There are many businessmen who like mostly vinyl as there promotional tool, not only because it is the latest trend in advertising, maybe because vinyl is very durable, versatile, and easy to work with. Because it’s durable, vinyl banners can be mounted, attached, or hung in several different ways, making it very versatile as well. The major advantages of vinyl to any other material is that is can be used conveniently both inside and outside for many different applications. From several different types of vinyl, we can come up to many kinds of vinyl banner with each one lending itself to better applications. Below are several applications where vinyl banners are commonly used: Trade Show Banners   Hanging Banners Exhibit Banners   Office Promotional Signs Large Format Vinyl Banners  New Housing Development Signs Sponsorship Banners   Festival Banners Welcome Signs   Street Pole Banners Point-of-Purchase Signs  Vinyl Window Signs Construction Site Signs    Banner Stands Street Banners    Vinyl Car Graphics</p>
+                    <p>In addition, listed below are the different types of vinyl that we can use to create vinyl banners, from small sizes to large format vinyl banners.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 )
 
 export default Home
